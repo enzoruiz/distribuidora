7a43a47e23bdc89b8dc74581037dad186c0cf590
enzo ruiz <enzo.rp.90@gmail.com>
2017-06-28 17:27:10 -0500

    primer modelo

--- apps/intranet/models.py
+++ apps/intranet/models.py
@@ -1,1 +1,11 @@
-from run import db
+from datetime import datetime
+from distribuidora.run import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(45), unique=True)
+    documento_identidad = db.Column(db.String(11), unique=True)
+    email = db.Column(db.String(35), unique=True)
+    password = db.Column(db.String(100), unique=True)
+    created_at = db.Column(db.DateTime, default=datetime.now)
--- run.py
+++ run.py
@@ -4,9 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_pyfile('config/local.py')
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 from apps.intranet.views import *
+from apps.intranet.models import *
 
 if __name__ == '__main__':
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
     app.run(port=8000)
